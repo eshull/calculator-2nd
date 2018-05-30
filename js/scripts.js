b4776a895e8f62252359fc41c8e666e958b7d912
Eric Shull and Thad Donaghue <eric@>
2018-05-30 16:52:11 -0700

    change empty div

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-var add = function(number1, number2) {
+// var add = function(number1, number2) {
+//   return number1 + number2;
+// };
+
+function add(number1, number2) {
   return number1 + number2;
 };
 
@@ -16,10 +20,12 @@ var quot = function(number1, number2){
 
 $(document).ready(function(){
   $("form#add").submit(function(event) {
+    event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
+    var result = add(number1,number2);
 
-    alert(add(number1, number2));
+    $("#output").text(result);
 
   });
 
